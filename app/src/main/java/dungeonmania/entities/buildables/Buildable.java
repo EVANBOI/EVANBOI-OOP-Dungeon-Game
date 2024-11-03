bca484bfa3fe46540bb348c6f36db2a8f681ff37
Evan Xiong <z5421149@ad.unsw.edu.au>
2024-11-03 23:13:31 +1100

    Task1 f: Abstracted the buildable logic in the inventory class

--- app/src/main/java/dungeonmania/entities/buildables/Buildable.java
+++ app/src/main/java/dungeonmania/entities/buildables/Buildable.java
@@ -1,10 +1,15 @@
 package dungeonmania.entities.buildables;
 
+import dungeonmania.entities.EntityFactory;
 import dungeonmania.entities.inventory.ConsumableInventoryItem;
+import dungeonmania.entities.inventory.Inventory;
 import dungeonmania.util.Position;
 
 public abstract class Buildable extends ConsumableInventoryItem {
     public Buildable(Position position) {
         super(position);
     }
+
+    public abstract Buildable buildItem(Inventory inventory, boolean remove, EntityFactory factory);
+
 }
