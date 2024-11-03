6c21a65aafdecda8825e4e2ae2b611156aa06440
Alex Wang <z5420393@ad.unsw.edu.au>
2024-11-03 21:20:47 +1100

    Task2F design in blog and test stubs

---
+++ app/src/test/java/dungeonmania/mvp/task2/LogicSwitchesTest.java
@@ -0,0 +1,49 @@
+// package dungeonmania.mvp.task2;
+
+// import dungeonmania.DungeonManiaController;
+// import dungeonmania.mvp.TestUtils;
+// import dungeonmania.response.models.DungeonResponse;
+// import dungeonmania.util.Direction;
+
+// import org.junit.jupiter.api.DisplayName;
+// import org.junit.jupiter.api.Test;
+
+// import static org.junit.jupiter.api.Assertions.*;
+
+// public class LogicSwitchesTest {
+//     @Test
+//     @DisplayName("Test logical entity activation by cardinally adjacent wire")
+//     public void wireLightBulbANDActivation();
+
+//     @Test
+//     @DisplayName("Test logical entity activation by cardinally adjacent switch")
+//     public void switchLightBulbORActivation();
+
+//     @Test
+//     @DisplayName("Test OR logical entity activation")
+//     public void orActivation();
+
+//     @Test
+//     @DisplayName("Test AND logical entity activation")
+//     public void andActivation();
+
+//     @Test
+//     @DisplayName("Test XOR logical entity activation")
+//     public void xorActivation();
+
+//     @Test
+//     @DisplayName("Test CO_AND logical entity activation")
+//     public void co_andActivation();
+
+//     @Test
+//     @DisplayName("Test logical bomb")
+//     public void logicalBomb();
+
+//     @Test
+//     @DisplayName("Test switch door")
+//     public void switchDoor();
+
+//     @Test
+//     @DisplayName("Test light bulb")
+//     public void lightBulb();
+// }
