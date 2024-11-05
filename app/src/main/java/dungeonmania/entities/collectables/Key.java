2fec4ac31c6f7ad8c3a1b4e58c99383d70479284
Alex Wang <z5420393@ad.unsw.edu.au>
2024-11-06 00:42:22 +1100

    Task3F: fixed keys so that a Player can only have 1 in inventory

--- app/src/main/java/dungeonmania/entities/collectables/Key.java
+++ app/src/main/java/dungeonmania/entities/collectables/Key.java
@@ -2,6 +2,7 @@ package dungeonmania.entities.collectables;
 
 import dungeonmania.entities.Entity;
 import dungeonmania.entities.OverlapBehaviour;
+import dungeonmania.entities.Player;
 import dungeonmania.entities.inventory.InventoryItem;
 import dungeonmania.map.GameMap;
 import dungeonmania.util.Position;
@@ -15,6 +16,11 @@ public class Key extends InventoryItem implements OverlapBehaviour {
     }
 
     public void onOverlap(GameMap map, Entity entity) {
+        if (entity instanceof Player) {
+            if (((Player) entity).countEntityOfType(Key.class) != 0) {
+                return;
+            }
+        }
         CollectableOnOverlap.handleOverlap(map, entity, this);
     }
 
