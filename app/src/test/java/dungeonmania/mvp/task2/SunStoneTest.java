2ec54e790a4d3eb13ff644e54e8743e249b998a4
Evan Xiong <z5421149@ad.unsw.edu.au>
2024-11-05 20:34:10 +1100

    Task2D: added test stubs and finished requirement analysis

---
+++ app/src/test/java/dungeonmania/mvp/task2/SunStoneTest.java
@@ -0,0 +1,31 @@
+package dungeonmania.mvp.task2;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SunStoneTest {
+    @Test
+    @DisplayName("Test sun stone opening door")
+    public void testSunStoneOpenDoor() {
+    };
+
+    @Test
+    @DisplayName("Test sun stone can build shield")
+    public void testSunStoneBuildShield() {
+    };
+
+    @Test
+    @DisplayName("Test that keys/treasure are used to build shield")
+    public void testSunStoneCraftingPriority() {
+    };
+
+    @Test
+    @DisplayName("Test sun stone cannot bribe mercenary")
+    public void testSunStoneBribeMercenary() {
+    };
+
+    @Test
+    @DisplayName("Test sun stone counts towards treasure goal")
+    public void testSunStoneTreasureGoal() {
+    };
+}
